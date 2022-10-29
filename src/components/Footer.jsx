e28dfae72e5e9222b30b25a42277b8c12e8fd6bb
AdiFischer <adifischer1@gmail.com>
2022-10-29 16:21:48 -0400

    cartoon

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return(
+        <footer>
+            <p>&copy; 2022 Adi Fischer.
+
+            </p>
+        </footer>
+    )
+}
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+        <header>
+            <h1>Awesome Cartoons</h1>
+        </header>
+    )
+}
+export default Header
